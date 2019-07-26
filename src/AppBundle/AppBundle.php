5d80d32772383a6f3463b664418616a861faa29e
mambzero <mambzero@abv.bg>
2019-07-26 18:22:21 +0300

    Set timezone

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -6,4 +6,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
+    public function boot()
+    {
+        parent::boot();
+        date_default_timezone_set("Europe/Athens");
+    }
 }
