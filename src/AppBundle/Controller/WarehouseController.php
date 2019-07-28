ebd5022a0eb4ce02af0ab9500654d20550c65690
mambzero <mambzero@abv.bg>
2019-07-28 17:12:00 +0300

    Created Warehouse Controller

---
+++ src/AppBundle/Controller/WarehouseController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Service\WarehouseServiceInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WarehouseController extends Controller
+{
+
+    private $warehouseService;
+
+    public function __construct(WarehouseServiceInterface $warehouseService)
+    {
+        $this->warehouseService = $warehouseService;
+    }
+
+    /**
+     * @Route("/stocks", name="stocks")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     *
+     * @return Response
+     */
+    public function listStocks()
+    {
+        $stocks = $this->warehouseService->listStocks();
+
+        return $this->render('warehouse/stocks.html.twig', [
+            'products' => $stocks
+        ]);
+    }
+
+}
