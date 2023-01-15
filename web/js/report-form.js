cd07980347be0ef5fb7447c5f7f835c7cc204015
mambzero <mambzero@abv.bg>
2023-01-15 09:43:14 +0200

    Report and Order entities dateAdded can be modified

--- web/js/report-form.js
+++ web/js/report-form.js
@@ -18,3 +18,11 @@ $('.chosen-plugin').chosen().on('change', function (e, params) {
 $('[data-toggle="tooltip"]').tooltip({
     trigger: 'hover'
 });
+
+$(document).ready(function() {
+    // you may need to change this code if you are not using Bootstrap Datepicker
+    $('.bootstrap-datepicker').datepicker({
+        format: 'yyyy-mm-dd',
+        autoclose: true
+    });
+});
